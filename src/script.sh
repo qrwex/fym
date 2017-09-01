bd1d650ed8fa582f7ca4f565f83e88b79e3b099f
Laimonas Jocys <laimonas.jocys@westernunion.com>
2017-09-01 10:16:34 +0300

    reformat

--- src/script.sh
+++ src/script.sh
@@ -7,90 +7,90 @@ stopVpn=0
 unlockProfile=0
 
 if [ $# -eq 0 ]; then
-	validArg=0
+    validArg=0
 fi
 
 # Loop through the args
 while [ $# -gt 0  -a  $validArg -eq 1 ]; do
-	validArg=0;
-	if [ $1 == "stop-antivirus" ];	then	stopAntivirus=1;	validArg=1;	fi
-	if [ $1 == "start-vpn" ];		then	startVpn=1;			validArg=1;	fi
-	if [ $1 == "stop-vpn" ];		then	stopVpn=1;			validArg=1;	fi
-	if [ $1 == "unlock-profile" ];	then	unlockProfile=1;	validArg=1;	fi
+    validArg=0;
+    if [ $1 == "stop-antivirus" ];	then	stopAntivirus=1;	validArg=1;	fi
+    if [ $1 == "start-vpn" ];		then	startVpn=1;			validArg=1;	fi
+    if [ $1 == "stop-vpn" ];		then	stopVpn=1;			validArg=1;	fi
+    if [ $1 == "unlock-profile" ];	then	unlockProfile=1;	validArg=1;	fi
 
-	if [ $validArg -eq 1 ]; then
-		shift
-	fi
+    if [ $validArg -eq 1 ]; then
+        shift
+    fi
 done
 
 Help()
 {
-	echo
-	echo "Usage: sudo sh script.sh	[stop-antivirus] [start-vpn] [stop-vpn] [unlock-profile]"
-	echo
+    echo
+    echo "Usage: sudo sh script.sh	[stop-antivirus] [start-vpn] [stop-vpn] [unlock-profile]"
+    echo
 }
 
 if [ "$EUID" -ne 0 ];	then
-	echo
-	echo "Please run as root"
-  	Help
-  	exit 1
+    echo
+    echo "Please run as root"
+    Help
+    exit 1
 fi
 
 if [ $validArg -eq 0 ]; then
-	Help
-	exit 1
+    Help
+    exit 1
 fi
 
 # Stop Mcafee
 StopAntiVirus()
 {
-	echo
-	echo "Stopping Mcafee AntiVirus"
-	launchctl remove com.mcafee.ssm.ScanManager
-	echo
+    echo
+    echo "Stopping Mcafee AntiVirus"
+    launchctl remove com.mcafee.ssm.ScanManager
+    echo
 }
 
 # Start BESAgentControlPanel
 StartVPN()
 {
-	echo
-	echo "Starting BESAgentControlPanel"
-	/Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -start
-	echo
+    echo
+    echo "Starting BESAgentControlPanel"
+    /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -start
+    echo
 }
 
 # Stop BESAgentControlPanel
 StopVPN()
 {
-	echo
-	echo "Stopping BESAgentControlPanel"
-	/Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -stop
-	sleep 2
-	launchctl stop net.pulsesecure.AccessService
-	echo
+    echo
+    echo "Stopping BESAgentControlPanel"
+    /Library/BESAgent/BESAgent.app/Contents/MacOS/BESAgentControlPanel.sh -stop
+    sleep 2
+    launchctl stop net.pulsesecure.AccessService
+    echo
 }
 
 # Enable disabled system preferences (Touch ID, iCloud etc.)
 # NOTE: unplug LAN cable.
 UnlockProfile()
 {
-	echo
-	echo "Unlocking profile"
-	MDM_UUID=$(profiles -Lv | awk '/attribute: name: MDM/,/attribute: profileUUID:/' | awk '/attribute: profileUUID:/ {print $NF}')
-	echo "MDM_UUID:" $MDM_UUID
+    echo
+    echo "Unlocking profile"
+    MDM_UUID=$(profiles -Lv | awk '/attribute: name: MDM/,/attribute: profileUUID:/' | awk '/attribute: profileUUID:/ {print $NF}')
+    echo "MDM_UUID:" $MDM_UUID
 
-	if [ -z "$MDM_UUID" ]; then
+    if [ -z "$MDM_UUID" ]; then
         echo "MDM profile NOT found. Attempting to manage"
         jamf manage
-	else
+    else
         echo "MDM profile found. Removing MDM before attempting to manage"
         profiles -R -p "$MDM_UUID"
         sleep 2
         jamf manage
-	fi
+    fi
 
-	echo "Disabling JAMF"
+    echo "Disabling JAMF"
     sleep 1
     launchctl remove com.jamfsoftware.task.Every\ 15\ Minutes
     launchctl remove com.jamfsoftware.jamf.daemon
@@ -100,21 +100,21 @@ UnlockProfile()
 }
 
 if [ $stopAntivirus -eq 1 ]; then
-	StopAntiVirus
+    StopAntiVirus
 fi
 
 
 if [ $startVpn -eq 1 ]; then
-	StartVPN
+    StartVPN
 fi
 
 if [ $stopVpn -eq 1 ]; then
-	StopVPN
+    StopVPN
 fi
 
 
 if [ $unlockProfile -eq 1 ]; then
-	UnlockProfile
+    UnlockProfile
 fi
 
 exit 0
