01849955c40ca3b74104b06bb9a3f05d02a4ab15
Laimonas Jocys <laimonas.jocys@westernunion.com>
2018-05-29 15:50:48 +0300

    update

--- src/script.sh
+++ src/script.sh
@@ -26,63 +26,38 @@ done
 Help()
 {
     echo
-    echo "Usage: sudo sh script.sh	[stop-antivirus] [start-vpn] [stop-vpn] [unlock-profile]"
+    echo "Usage: sh script.sh	[stop-antivirus] [start-vpn] [stop-vpn] [unlock-profile]"
     echo
 }
-
-if [ "$EUID" -ne 0 ];	then
-    echo
-    echo "Please run as root"
-    Help
-    exit 1
-fi
 
 if [ $validArg -eq 0 ]; then
     Help
     exit 1
 fi
 
-# Stop Mcafee
+# Stop McAfee
 StopAntiVirus()
 {
-    echo
-    echo "Stopping Mcafee AntiVirus"
     launchctl remove com.mcafee.ssm.ScanManager
-    echo
 }
 
 # Start Pulse Secure
 StartVPN()
 {
-    echo
-    echo "Stopping Pulse Secure Agent"
     launchctl load /Library/LaunchAgents/net.pulsesecure.pulsetray.plist
-    sleep 2
-    echo "Started!"
-    echo
 }
 
 # Stop Pulse Secure
 StopVPN()
 {
-    echo
-    echo "Stopping Pulse Secure Agent"
     launchctl unload /Library/LaunchAgents/net.pulsesecure.pulsetray.plist
-    sleep 2
-    echo "Stopped!"
-    echo
 }
 
 # Enable disabled system preferences (Touch ID, iCloud etc.)
 # NOTE: unplug LAN cable.
 UnlockProfile()
 {
-    echo
-    echo "Unlocking profile"
     /usr/local/bin/jamf removeFramework
-    sleep 2
-    echo "Unlocked!"
-    echo
 }
 
 if [ $stopAntivirus -eq 1 ]; then
