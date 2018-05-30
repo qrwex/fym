89c924b6cab502ab0800d04bb2375b66770b780b
Laimonas Jocys <laimonas.jocys@westernunion.com>
2018-05-30 14:30:53 +0300

    update

--- src/script.sh
+++ src/script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 validArg=1
-stopAntivirus=0
+stopAntiVirus=0
+startAntiVirus=0
 startVpn=0
 stopVpn=0
 unlockProfile=0
@@ -13,7 +14,8 @@ fi
 # Loop through the args
 while [ $# -gt 0  -a  $validArg -eq 1 ]; do
     validArg=0;
-    if [ $1 == "stop-antivirus" ];  then    stopAntivirus=1;    validArg=1;	fi
+    if [ $1 == "stop-av" ];         then    stopAntiVirus=1;    validArg=1;	fi
+    if [ $1 == "start-av" ];        then    startAntiVirus=1;   validArg=1;	fi
     if [ $1 == "start-vpn" ];       then    startVpn=1;         validArg=1;	fi
     if [ $1 == "stop-vpn" ];        then    stopVpn=1;          validArg=1;	fi
     if [ $1 == "unlock-profile" ];  then    unlockProfile=1;    validArg=1;	fi
@@ -26,7 +28,7 @@ done
 Help()
 {
     echo
-    echo "Usage: sh script.sh	[stop-antivirus] [start-vpn] [stop-vpn] [unlock-profile]"
+    echo "Usage: sh script.sh	[start-av] [stop-av] [start-vpn] [stop-vpn] [unlock-profile]"
     echo
 }
 
@@ -38,7 +40,15 @@ fi
 # Stop McAfee
 StopAntiVirus()
 {
-    launchctl remove com.mcafee.ssm.ScanManager
+    launchctl unload /Library/LaunchAgents/com.mcafee.menulet.plist
+    launchctl unload /Library/LaunchAgents/com.mcafee.reporter.plist
+}
+
+# Start McAfee
+StartAntiVirus()
+{
+    launchctl load /Library/LaunchAgents/com.mcafee.menulet.plist
+    launchctl load /Library/LaunchAgents/com.mcafee.reporter.plist
 }
 
 # Start Pulse Secure
@@ -54,14 +64,17 @@ StopVPN()
 }
 
 # Enable disabled system preferences (Touch ID, iCloud etc.)
-# NOTE: unplug LAN cable.
 UnlockProfile()
 {
     /usr/local/bin/jamf removeFramework
 }
 
-if [ $stopAntivirus -eq 1 ]; then
+if [ $stopAntiVirus -eq 1 ]; then
     StopAntiVirus
+fi
+
+if [ $startAntiVirus -eq 1 ]; then
+    StartAntiVirus
 fi
 
 
